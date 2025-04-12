1c6dfb2da5f724ecb987446b1de9e65490f9a47b
Louis Venhoff <Louis.venhoff@gmx.net>
2025-04-12 17:46:44 +0200

    Worked on header

---
+++ louis_venhoff_portfolio/src/staticContent/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/staticContent/header.css";
+
+const Header:React.FC = () => {
+    return(
+        <div className="header--main">
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default Header;
