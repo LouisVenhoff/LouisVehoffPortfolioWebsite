cd1bf103d749bd6d8b8af98109240217e666af6d
Louis Venhoff <Louis.venhoff@gmx.net>
2025-04-18 12:15:33 +0200

    Added homepage introduction

--- louis_venhoff_portfolio/src/pages/home/home.tsx
+++ louis_venhoff_portfolio/src/pages/home/home.tsx
@@ -5,10 +5,21 @@ const Home:React.FC = () => {
     <div className="absolute flex justify-center items-center w-full h-full">
         <div className="home--content">
             <div className="home--introduction">
-                
+               <p className="home-introduction--heading">
+                    Hi, <br />
+               </p>
+               <strong className="home-introduction--heading">
+                    i´m
+               </strong>
+               <strong className="home-introduction--heading home-introduction-description--focus-name">
+                    {" Louis"}
+               </strong>
+               <p className="home-introduction--heading">
+                    FullStack Developer <br />
+               </p>
             </div>
             <div className="home--avatar">
-                
+
             </div>
         </div>
     </div>);
