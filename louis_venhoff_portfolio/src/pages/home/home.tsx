112233a978bd85f0c0f919d2ae27201dfd30c5f2
Louis Venhoff <Louis.venhoff@gmx.net>
2025-04-21 12:29:42 +0200

    restructured home page

--- louis_venhoff_portfolio/src/pages/home/home.tsx
+++ louis_venhoff_portfolio/src/pages/home/home.tsx
@@ -2,28 +2,31 @@ import CommitHeatmap from "../../components/commitHeatmap/commitHeatmap";
 import "../../styles/pages/home/home.css";
 import avatar from "../../assets/louis.png";
 
-
-const Home:React.FC = () => {
-      
-    return(
-    <div className="absolute flex justify-center items-center w-full h-full">
-        <div className="home--content">
-            <div className="home--introduction">
+const Home: React.FC = () => {
+  return (
+    <>
+     <div className="flex justify-center items-center w-full h-full">
+          <div className="home--content">
+          <div className="home--introduction">
                <div className="home-introduction--heading">
-                    <span>
-                         Hi, <br /> i'm <span className="home-introduction-description--focus-name"> {" Louis \r\n"} <br /> </span> FullStack Developer
-                    </span>
+               <span>
+               Hi, <br /> i'm{" "}
+               <span className="home-introduction-heading--focus-name">
+                    {" "}
+                    {" Louis \r\n"} <br />{" "}
+               </span>{" "}
+               FullStack Developer
+               </span>
                </div>
-               
                <CommitHeatmap />
-               
-            </div>
-            <div className="home--avatar">
-               <img src={avatar} width="500" />
+          </div>
+          <div className="home--avatar">
+               <img src={avatar} className="home-avatar--image" width="500px" />
+          </div>
           </div>
      </div>
-     </div>
-    );
-}
+    </>
+  );
+};
 
 export default Home;
