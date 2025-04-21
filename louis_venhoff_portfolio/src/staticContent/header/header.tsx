7071e96854800f2764bb36b09fb0675640ec0589
Louis Venhoff <Louis.venhoff@gmx.net>
2025-04-21 12:59:04 +0200

    Add header prototype

--- louis_venhoff_portfolio/src/staticContent/header/header.tsx
+++ louis_venhoff_portfolio/src/staticContent/header/header.tsx
@@ -1,6 +1,7 @@
 import React, {JSX, ReactNode, useEffect, useState} from "react";
 import "../../styles/staticContent/header.css";
-import NavButton from "./navButton/navButton";
+import { Button, Drawer, Portal } from "@chakra-ui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 type HeaderProps = {
     children:ReactNode,
@@ -10,6 +11,7 @@ type HeaderProps = {
 const Header:React.FC<HeaderProps> = ({ children }) => {
         
     return(
+        <>
         <div className="header--main">
             <div className="header--title">
                 <h1>Louis Venhoff</h1>
@@ -18,11 +20,34 @@ const Header:React.FC<HeaderProps> = ({ children }) => {
                 <div className="header--actions">
                     {children}
                 </div>
-                <div className="header-actions--underline">
-
-                </div>
+                <div className="header-actions--underline" />
             </div>
         </div>
+        <div className="header--drawer">
+            <Drawer.Root placement="start">
+                <Drawer.Trigger asChild>
+                    <Button variant="plain" color="teal">
+                        <GiHamburgerMenu />        
+                    </Button>
+                </Drawer.Trigger>
+                <Portal>
+                    <Drawer.Backdrop />
+                    <Drawer.Positioner>
+                        <Drawer.Content backgroundColor="black">
+                            <Drawer.Header>
+                                <Drawer.Title>
+                                    Menu
+                                </Drawer.Title>
+                            </Drawer.Header>
+                            <Drawer.Body>
+                                {children}
+                            </Drawer.Body>
+                        </Drawer.Content>
+                    </Drawer.Positioner>
+                </Portal>
+            </Drawer.Root>
+        </div>
+        </>
     );
 }
 
