13584243c172d1fdc99bb928effdb769aca9383f
Louis Venhoff <Louis.venhoff@gmx.net>
2025-04-26 19:47:34 +0200

    Worked on markdown

--- louis_venhoff_portfolio/src/pages/project/project.tsx
+++ louis_venhoff_portfolio/src/pages/project/project.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../styles/pages/project/project.css";
+import ReactMarkdown from "react-markdown";
+import test from "../../assets/test.md?raw";
 
-const Project:React.FC = () => {
-    return(<>Hello World!</>);
+const Project:React.FC = () => { 
+    return(
+        <div className="w-full h-full flex justify-center">
+            <div className="bg-black m-64 w-full h-full">
+                <ReactMarkdown>
+                    {test}
+                </ReactMarkdown>
+            </div>
+        </div>
+    );
 }
 
 export default Project;
