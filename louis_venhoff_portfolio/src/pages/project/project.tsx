af1111ff2691f275414d74d6a2db430dfa5f635c
Louis Venhoff <Louis.venhoff@gmx.net>
2025-04-27 12:28:05 +0200

    Rearrange header

--- louis_venhoff_portfolio/src/pages/project/project.tsx
+++ louis_venhoff_portfolio/src/pages/project/project.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../../styles/pages/project/project.css";
-import MarkdownPreview from "@uiw/react-markdown-preview";
-import test from "../../assets/test.md?raw";
+import MarkdownElement from "../../components/markdownElement/markdownElement";
 
 const Project:React.FC = () => { 
     return(
-        <div className="project--main">
-            <div className="project--content">
-                "Hello WOrld"
+        <>
+            <div className="project-main">
+                <div className="project-main--header">
+                    Dillinger, the nice Markdown editor!
+                </div>
+                <div className="project-main--markdown-viewer">
+                    <MarkdownElement />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
