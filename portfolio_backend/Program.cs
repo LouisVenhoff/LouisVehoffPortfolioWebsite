de99bee75421dc31fc6cb174132397322cf37f7b
Louis Venhoff <Louis.venhoff@gmx.net>
2025-05-01 13:21:01 +0200

    backend init

--- portfolio_backend/Program.cs
+++ portfolio_backend/Program.cs
@@ -1,3 +1,5 @@
+using portfolio_backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+Console.WriteLine(connectionString);
+builder.Services.AddDbContext<ApplicationDbContext>((options) => {
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
