677e52a99996d17c39e21453bf03359a558a89fc
Louis Venhoff <Louis.venhoff@gmx.net>
2025-05-11 02:07:08 +0200

    Worked on pull function

--- portfolio_backend/lib/libGit2SharpWrapper.cs
+++ portfolio_backend/lib/libGit2SharpWrapper.cs
@@ -1,6 +1,8 @@
 
 
 using LibGit2Sharp;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using NuGet.Protocol.Plugins;
 
 namespace portfolio_backend.Lib;
 
@@ -12,13 +14,15 @@ class LibGit2SharpWrapper{
 
     private FetchOptions fetchOptions;
 
+    private string githubToken;
+
+
 
     public LibGit2SharpWrapper(string githubToken){
 
-        this.credentials = new UsernamePasswordCredentials{
-            Username = "git",
-            Password = githubToken
-        };
+        this.githubToken = githubToken;
+
+        this.credentials = LibGit2SharpWrapper.GetCredentials(this.githubToken);
 
         this.fetchOptions = new FetchOptions{
             CredentialsProvider= (_url, _user, _cred) => this.credentials
@@ -29,6 +33,40 @@ class LibGit2SharpWrapper{
 
     public string Clone(string cloneUrl, string targetFolder){
         return Repository.Clone(cloneUrl, targetFolder, this.cloneOptions);
+    }
+
+    public bool Checkout(Repository repo){
+        var branch = repo.Branches["docs"];
+
+        if(branch == null) return false;
+
+        Commands.Checkout(repo, branch);
+
+        return true;
+    }
+
+    public void Pull(Repository repo){
+        
+        PullOptions pullOptions = new PullOptions{
+            
+            FetchOptions = new FetchOptions{
+                CredentialsProvider = (_, _, _) => GetCredentials(this.githubToken),
+            },
+            MergeOptions = new MergeOptions{
+            FastForwardStrategy = FastForwardStrategy.FastForwardOnly
+            }
+        };
+        
+        Commands.Pull(repo, new Signature("auto_portfolio", "auto_portfolio@portfolio.net", DateTimeOffset.Now), pullOptions);
+
+    }
+
+
+    private static UsernamePasswordCredentials GetCredentials(string githubToken){
+         return new UsernamePasswordCredentials{
+            Username = "git",
+            Password = githubToken
+        };
     }
 
 
