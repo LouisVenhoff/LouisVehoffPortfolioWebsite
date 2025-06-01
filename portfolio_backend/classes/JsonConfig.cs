86be9620f2920c9d5b54d1abc8bd1eeade2f5223
Louis Venhoff <Louis.venhoff@gmx.net>
2025-06-01 17:52:00 +0200

    Added loading the config file

---
+++ portfolio_backend/classes/JsonConfig.cs
@@ -0,0 +1,12 @@
+namespace portfolio_backend.Classes
+{
+    class JsonConfig
+    {
+        public string DocumentName { get; set; }
+
+        public string Description { get; set; }
+
+        public List<String> Tags { get; set; }
+
+    }    
+}
