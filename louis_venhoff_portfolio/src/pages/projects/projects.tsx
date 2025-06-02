bbfbca9827a1d4eb70b3f0e800676fcd5baf52c8
Louis Venhoff <Louis.venhoff@gmx.net>
2025-06-02 23:54:18 +0200

    Show thumbnail loaded from server

--- louis_venhoff_portfolio/src/pages/projects/projects.tsx
+++ louis_venhoff_portfolio/src/pages/projects/projects.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import ProjectThumbnail from "../../components/projectThumbnail/projectThumbnail";
+import useDocument from "../../hooks/useDocument";
 import "../../styles/pages/projects/projects.css";
 
 
 const Projects:React.FC = () => {
+    
+    const [docs, setDocs] = useState<Doc[]>([]);
+    
+    const documentTools = useDocument();
+
+    useEffect(() => {
+        documentTools.loadDocumentList().then(e => setDocs(e));
+    });
+
+    const buildThumbnails = ():JSX.Element[] => {
+        
+        return docs.map((doc: Doc) => {
+            
+            let imagePath:string = `http://localhost:5297/api/Docs/download/thumbnail/${doc.docId}`
+            
+            return <ProjectThumbnail name={doc.name} description={doc.description} tags={doc.tags} imagePath={imagePath} projectId={doc.docId} />
+        });
+    }
+    
+    
     return (
         <div className="flex justify-center">
             <div className="projects--container">
-                <ProjectThumbnail name="TestEintrag" description="Ein testeintrag für dieses Portfolio" tags={["Tag1", "Tag2", "Tag3", "Tag4", "hasdjashkdajshd", "hajasdkhas"]} imagePath={"http://localhost:5297/api/Docs/download/thumbnail/1"} projectId={0}/>
+                {buildThumbnails()}
             
             </div>
         </div>
