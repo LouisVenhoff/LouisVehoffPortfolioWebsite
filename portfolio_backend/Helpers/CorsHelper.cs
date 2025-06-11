6e37b3822f8cdcb1c98bcb58f877663d72edd8ce
Louis Venhoff <Louis.venhoff@gmx.net>
2025-06-11 22:34:38 +0200

    Added cors setup and helper

---
+++ portfolio_backend/Helpers/CorsHelper.cs
@@ -0,0 +1,11 @@
+namespace portfolio_backend.Helpers
+{
+    static class CorsHelper
+    {
+        public static string[] GetAllowedOrigins()
+        {
+            return ["http://digitaldevdreams.site", "http://87.106.127.217"];
+        }
+    }
+}
+
